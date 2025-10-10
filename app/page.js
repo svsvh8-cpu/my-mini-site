d967536f1a922a50f26ec9eaee7a583556f45f18
Player <Player>
2025-10-10 20:20:54 +0400

    about: test change in dev

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <section style={{ padding: '24px' }}>
-      <h1>Welcome to My Mini Site</h1>
+      <h1>Welcome to My Mini Site HER</h1>
       <p>This is the Home page.</p>
 
       <nav style={{ marginTop: '16px', display: 'flex', gap: '12px' }}>
